1eb361575911bfb046d1a46558150fffe9cd3e30
Ilias Ismanalijev <me@il.ly>
2021-06-01 17:07:54 +0200

    custom styling for Officient

--- web/app_options.js
+++ web/app_options.js
@@ -244,7 +244,7 @@ const defaultOptions = {
     value:
       typeof PDFJSDev === "undefined" || !PDFJSDev.test("PRODUCTION")
         ? "../src/worker_loader.js"
-        : "../build/pdf.worker.js",
+        : "/pdfjs/build/pdf.worker.js",
     kind: OptionKind.WORKER,
   },
 };
@@ -267,7 +267,7 @@ if (
     value:
       typeof PDFJSDev === "undefined" || !PDFJSDev.test("PRODUCTION")
         ? "../build/dev-sandbox/pdf.sandbox.js"
-        : "../build/pdf.sandbox.js",
+        : "/pdfjs/build/pdf.sandbox.js",
     kind: OptionKind.VIEWER,
   };
 } else if (PDFJSDev.test("CHROME")) {
@@ -278,7 +278,7 @@ if (
   };
   defaultOptions.sandboxBundleSrc = {
     /** @type {string} */
-    value: "../build/pdf.sandbox.js",
+    value: "/pdfjs/build/pdf.sandbox.js",
     kind: OptionKind.VIEWER,
   };
 }
